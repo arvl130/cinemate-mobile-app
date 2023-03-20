570651d8d2b1330b01299cb30d2b6ac058687e52
Angelo Geulin <angelogeulin123@gmail.com>
2023-03-20 12:46:20 +0800

    feat: enable loading movies in Movie Details screen

--- src/screens/HomeTab/MovieDetails.tsx
+++ src/screens/HomeTab/MovieDetails.tsx
@@ -1,27 +1,86 @@
-import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 import { LinearGradient } from "expo-linear-gradient"
-import { Ionicons } from "@expo/vector-icons"
-import {
-  Dimensions,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native"
-import { useNavigation } from "@react-navigation/native"
+import { Dimensions, Image, ScrollView, Text, View } from "react-native"
+import { useEffect, useState } from "react"
+import { GetMovieResult } from "../../types/Movie"
 
 const { height } = Dimensions.get("window")
 
+function ShortInfo({ movie }: { movie: GetMovieResult }) {
+  return (
+    <View className="flex-[0] gap-6 flex-row">
+      <View className="flex-[2]">
+        <Image
+          source={{
+            uri: `https://image.tmdb.org/t/p/original/${movie.poster_path}`,
+          }}
+          className="h-48 w-32 rounded-2xl mb-3"
+        />
+      </View>
+      <View className="flex-[3]">
+        <Text className="text-white font-semibold text-xl mb-1">
+          {movie.title}
+        </Text>
+        <View className="flex-row flex-wrap gap-2 mb-1">
+          {movie.genres.map((genre) => (
+            <Text
+              key={genre.id}
+              className="bg-gray-500 rounded-full text-white text-xs px-2 py-1"
+            >
+              {genre.name}
+            </Text>
+          ))}
+        </View>
+        <View className="mb-3">
+          <Text className="text-gray-400">
+            {new Date(movie.release_date).getFullYear()}
+            {" • "}
+            {movie.original_language.toUpperCase()}
+            {" • "}
+            {movie.runtime} min
+          </Text>
+        </View>
+        <View>
+          <Text className="text-center">⭐⭐⭐⭐</Text>
+          <Text className="text-gray-400 text-xs text-center">
+            From 123 users
+          </Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+function LongInfo({ movie }: { movie: GetMovieResult }) {
+  return (
+    <View>
+      <Text className="text-white font-semibold text-base">Overview</Text>
+      <Text className="text-white">{movie.overview}</Text>
+      <Text className="text-white font-semibold text-base mt-3">
+        Ratings & Reviews
+      </Text>
+      <Text className="text-white">
+        Ratings and reviews are from people who have watched the movie.
+      </Text>
+    </View>
+  )
+}
+
 export function MovieDetailsScreen({ route }: any) {
   const { id } = route.params
-  const navigation = useNavigation<
-    NativeStackNavigationProp<{
-      Home: undefined
-      Search: {
-        query: string
-      }
-    }>
-  >()
+  const [movie, setMovie] = useState<GetMovieResult | null>(null)
+
+  async function getMovie(movieId: string) {
+    const apiKey = ""
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`
+    )
+    const movie = await response.json()
+    setMovie(movie)
+  }
+
+  useEffect(() => {
+    getMovie(id)
+  }, [])
 
   return (
     <View>
@@ -32,23 +91,20 @@ export function MovieDetailsScreen({ route }: any) {
           height,
         }}
       />
-      <ScrollView>
-        <View className="flex-row py-3 px-3 h-14">
-          <TouchableOpacity
-            activeOpacity={0.5}
-            onPress={() => {
-              navigation.goBack()
-            }}
-          >
-            <Ionicons name="arrow-back" size={32} color="white" />
-          </TouchableOpacity>
+      {movie ? (
+        <>
+          <ScrollView>
+            <View className="px-6">
+              <ShortInfo movie={movie} />
+              <LongInfo movie={movie} />
+            </View>
+          </ScrollView>
+        </>
+      ) : (
+        <View>
+          <Text>Loading ...</Text>
         </View>
-
-        <View className="h-14 flex-row justify-center items-center">
-          <Text className=" text-white text-lg">Movie Details</Text>
-          <Text className="text-white">Current movie ID: {id}</Text>
-        </View>
-      </ScrollView>
+      )}
     </View>
   )
 }
