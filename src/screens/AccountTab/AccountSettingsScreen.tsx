a84ab5ac996e041dfc18827adc1f7c80b3fc5d98
Angelo Geulin <angelogeulin123@gmail.com>
2023-04-25 14:44:45 +0800

    feat: add Account Settings screen

--- src/screens/AccountTab/AccountSettingsScreen.tsx
+++ src/screens/AccountTab/AccountSettingsScreen.tsx
@@ -1,34 +1,117 @@
+import React, { useState } from "react"
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Dimensions,
+} from "react-native"
 import { LinearGradient } from "expo-linear-gradient"
 import { signOut } from "firebase/auth"
-import { Dimensions, Text, TouchableOpacity, View } from "react-native"
 import { auth } from "../../firebase"
 
 const { height } = Dimensions.get("window")
 
 export function AccountSettingsScreen() {
+  const [isModalVisible, setisModalVisible] = useState(false)
+
+  const changeModalVisible = (bool: boolean) => {
+    setisModalVisible(bool)
+  }
+  const closeModal = (bool: boolean) => {
+    changeModalVisible(bool)
+  }
   return (
-    <View>
-      <LinearGradient
-        colors={["#000000", "#393737"]}
-        className="absolute left-0 right-0 top-0 bottom-0"
-        style={{
-          height,
-        }}
-      />
-      <View className="h-14 flex-row justify-center items-center">
-        <Text className=" text-white text-lg">Account Settings</Text>
+    <SafeAreaView>
+      <View>
+        <LinearGradient
+          colors={["#000000", "#393737"]}
+          className="absolute left-0 right-0 top-0 bottom-0"
+          style={{
+            height,
+          }}
+        />
       </View>
-      <View className="px-3">
-        <TouchableOpacity
-          activeOpacity={0.8}
-          className="bg-red-500 rounded-md"
-          onPress={() => signOut(auth)}
-        >
-          <Text className="text-white text-center px-4 py-3 uppercase font-medium">
-            Logout
-          </Text>
+
+      <View className="px-6">
+        <View className="flex-row items-center justify-center">
+          <View className="items-center">
+            <Image
+              source={{
+                uri: "https://www.angelogeulin.me/assets/profile-picture.ca024b3e.webp",
+              }}
+              className="h-24 w-24 rounded-full"
+            ></Image>
+            <TouchableOpacity activeOpacity={0.8} className="mt-2">
+              <Text className="text-blue-500 underline text-xs">
+                Edit Picture
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Text className="text-white font-medium [color:_#7E7979] mb-1">
+          Name
+        </Text>
+        <TextInput
+          placeholderTextColor={"white"}
+          value={auth.currentUser?.displayName as string}
+          className="[background-color:_#353535] rounded-md px-4 py-1 text-white"
+        />
+        <Text className="text-white font-medium [color:_#7E7979] mt-3 mb-1">
+          Email Address
+        </Text>
+        <TextInput
+          placeholderTextColor={"white"}
+          value={auth.currentUser?.email as string}
+          className="[background-color:_#353535] rounded-md px-4 py-1 text-white"
+        />
+        <Text className="text-white font-medium [color:_#7E7979] mt-3 mb-1">
+          Password
+        </Text>
+        <TextInput
+          placeholderTextColor={"white"}
+          placeholder={"********"}
+          className="[background-color:_#353535] rounded-md px-4 py-1"
+          secureTextEntry={true}
+        />
+        <Text className="text-white font-medium [color:_#7E7979] mt-6 mb-1">
+          Connected Account
+        </Text>
+        <TouchableOpacity activeOpacity={0.8}>
+          <View className="[background-color:_#353535] flex-row flex-wrap px-3 py-2 rounded-md justify-between items-center">
+            <View className="flex-row">
+              <View className="flex-row items-center">
+                <Image
+                  className="h-10 w-10"
+                  source={require("../../assets/social-media-icons/google.png")}
+                />
+              </View>
+              <View className="pl-2 justify-center">
+                <Text className="text-white font-medium">Google</Text>
+                <Text className="[color:_#848484] text-xs">
+                  {auth.currentUser?.email}
+                </Text>
+              </View>
+            </View>
+            <View>
+              <Text className="text-blue-500 underline text-xs">Remove</Text>
+            </View>
+          </View>
         </TouchableOpacity>
+        <View className="mt-6">
+          <TouchableOpacity
+            activeOpacity={0.8}
+            className="bg-red-500 rounded-md"
+            onPress={() => signOut(auth)}
+          >
+            <Text className="text-white text-center px-4 py-3 uppercase font-medium">
+              Logout
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
